78d601404dd5235ff865fd1081d5dd617f2f9dd1
oscar <oscarollie366@gmail.com>
2026-03-03 06:46:01 +0300

    Setup Frontend with React vite

---
+++ incident-frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-3xl font-bold text-blue-600'>Hello World</h1>
+    </>
+  )
+}
+
+export default App
